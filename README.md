9ca07b80d7f41ea6b3e6099e2182f3c89c77b0b1
Firas Medini <mdnfiras@yahoo.com>
2021-07-24 10:38:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Do you like my resume?
+
+You can freely steal my code and use it for your own benefit :D
+
---
+++ topdf.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+echo gg
+chromium --headless  --disable-gpu --print-to-pdf=out.pdf resume.html 
+
